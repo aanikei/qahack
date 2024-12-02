89c9fbe4c0377793e1cf14a8e40cb4d41aab7a5c
Artur Anikeich <65artur65@gmail.com>
2024-12-02 08:49:50 +0000

    Tests: final test_avatars

---
+++ images/image_generator.py
@@ -0,0 +1,46 @@
+from PIL import Image, ImageDraw
+import io
+import random
+
+def generate_image(size_b, ext='png'):
+  # Calculate number of pixels to achieve the required size
+  height = 1
+  if ext == 'png':
+    size = size_b // 4
+    if size == 0:
+      size = 1
+  elif ext == 'jpeg':
+    height = 480
+    size = size_b
+    if size == 0:
+      size = 1        
+
+  image = Image.new('RGB', (height, size), color='blue')
+
+  if ext == 'jpeg':
+    draw_image = ImageDraw.Draw(image)
+    for i in range(height):
+      for j in range(size):
+        draw_image.point((i, j), fill=(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))              )
+
+  # Save the image
+  with io.BytesIO() as output:
+    image.save(output, format=ext, compress_level=0, quality=100)
+
+    with open(str(size_b) + '.' + ext, 'wb') as f:
+      f.write(output.getvalue())
+
+# not exact size
+# generate_image(100, 'png')
+# generate_image(10000, 'png')
+# generate_image(2097152 - 772 + 16, 'png') #2096396, actual 2097151
+# generate_image(2097152 - 772 + 20, 'png') #2096400, actual 2097155
+generate_image(5000000, 'png')
+
+# generate_image(100, 'jpeg')
+# generate_image(1000, 'jpeg')
+# generate_image(2000, 'jpeg')
+# generate_image(2210, 'jpeg')
+# generate_image(2209, 'jpeg')
+# generate_image(2208, 'jpeg')
+generate_image(5000, 'jpeg')
--- test_avatars.py
+++ test_avatars.py
@@ -2,16 +2,18 @@ import requests
 import allure
 import pytest
 from data import *
+from schemas import user_schema
+from jsonschema import validate
 
-# all files are taken from https://sample-videos.com
+# pytest.skip(allow_module_level=True)
 
 task_id = 'api-11'
 @allure.title('put /users/{user_uuid}/avatar')
-@allure.description('Verify avatar can be added to a user')
+@allure.description('Verify avatar can be added to a user using valid images')
 @allure.testcase('D-1')
-@pytest.mark.parametrize("test_data, file_data", [(post_users_valid[0], item) for item in put_images])
+@pytest.mark.parametrize("test_data, file_data", [(post_users_valid[0], item) for item in put_images_valid])
 # @pytest.mark.skip()
-def test_put_users_uuid_avatar(test_data, file_data, url):
+def test_put_users_uuid_avatar_valid(test_data, file_data, url):
   headers = auth_header | {'X-Task-Id': task_id}
   delete_users(url, headers)
   new_user = create_user(url, headers, test_data)
@@ -20,15 +22,46 @@ def test_put_users_uuid_avatar(test_data, file_data, url):
 
   response = requests.put(f'{url}/users/{new_user['uuid']}/avatar', headers=headers, 
                                       files=files)
-  data = response.json()
+  header_data = response.headers
+  assert header_data['content-type'] == 'application/json'
+  assert int(header_data['content-length']) == len(response.content)
+  assert response.status_code == 200
 
+  data = response.json()
+  
+  assert 'https://gravatar.com/avatar/' in data['avatar_url']
+  validate(instance=data, schema=user_schema)
+
+
+task_id = 'api-11'
+@allure.title('put /users/{user_uuid}/avatar')
+@allure.description('Verify avatar will not be added to a user using invalid images')
+@allure.testcase('D-2')
+@pytest.mark.parametrize("test_data, file_data", [(post_users_valid[0], item) for item in put_images_invalid])
+# @pytest.mark.skip()
+def test_put_users_uuid_avatar_invalid(test_data, file_data, url):
+  headers = auth_header | {'X-Task-Id': task_id}
+  delete_users(url, headers)
+  new_user = create_user(url, headers, test_data)
+
+  files = {'avatar_file': open(fr'./images/{file_data['image']}', 'rb')}
+
+  response = requests.put(f'{url}/users/{new_user['uuid']}/avatar', headers=headers, 
+                                      files=files)
+  header_data = response.headers
+  assert int(header_data['content-length']) == len(response.content)
   assert response.status_code == file_data['code']
 
-  if file_data['code'] == 200:
-    assert 'https://' in data['avatar_url'] and 'avatar' in data['avatar_url']
-  else:
-    assert file_data['error'] in data['errorMessage']
+  data = response.json()  
 
-    response = requests.get(f'{url}/users/{new_user['uuid']}', headers=headers)
-    data = response.json()
-    assert '' == data['avatar_url']
+  if response.status_code == 431:
+    assert file_data['error'] in data['errorMessage']
+    assert header_data['content-type'] == 'application/json; charset=utf-8'
+  elif response.status_code == 400:
+    assert file_data['error'] in data['message']
+    assert header_data['content-type'] == 'application/json'
+
+  response = requests.get(f'{url}/users/{new_user['uuid']}', headers=headers)
+  data = response.json()
+  assert '' == data['avatar_url']
+  validate(instance=data, schema=user_schema)
